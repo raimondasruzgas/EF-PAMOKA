920b85109651e9f4456236f405fe156e196876e9
Aiwiux <raimondasruzgas@users.noreply.github.com>
2022-01-24 21:32:58 +0200

    Patoblinimai
    * Pridėta login vartotojo išlaikymas neatsijungus
    * Autotifikacija
    * Registracja:
    a. el.pasto validacija
    b. password validacija

--- frontend/projektas/src/app/api.service.ts
+++ frontend/projektas/src/app/api.service.ts
@@ -4,14 +4,28 @@ import { Router } from '@angular/router';
 
 
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private router : Router) { }
-
+  constructor(private router : Router) { 
+    let browserToken = localStorage.getItem('token')
+    if(browserToken != null){
+      this.loggedIn = true;
+      this.token = browserToken;
+    }
+  }
+  
+  loggedIn = false;
   token = ''
+
+  atsijungti(){
+    this.loggedIn = false;
+    localStorage.removeItem('token');
+    this.router.navigate(["login"]);
+  }
 
   async prisijunti(data: any) {
     data.Id = 0
@@ -35,8 +49,45 @@ export class ApiService {
     
     if (tokenas != null && atsakymasIsServerio.status == 200) {
       console.log(tokenas)
+      this.loggedIn = true;
+      
+      this.token = tokenas;
+      localStorage.setItem('token', this.token)
+
       this.router.navigate(['main'])
     }
 
   }
+
+  async registruotis(data: any) {
+    data.Id = 0
+    
+    let atsakymasIsServerio = await fetch('https://localhost:44321/register', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    
+
+    let tokenas = await atsakymasIsServerio?.json()
+
+    if (atsakymasIsServerio.status == 401){
+      alert("Toks vartotojas jau egzistuoja")
+    }
+    
+    if (tokenas != null && atsakymasIsServerio.status == 200) {
+      console.log(tokenas)
+      this.loggedIn = true;
+
+      this.token = tokenas;
+      localStorage.setItem('token', this.token)
+
+      this.router.navigate(['main'])
+    }
+
+  }
+
 }
--- frontend/projektas/src/app/page/register/register.component.ts
+++ frontend/projektas/src/app/page/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  forma = new FormGroup({
+    vardas: new FormControl('', Validators.required),
+    slaptazodis: new FormControl('', Validators.required),
+    slaptazodis2: new FormControl('', Validators.required),
+    pastas: new FormControl('', [Validators.required, Validators.email])
+  })
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+  }
+  registruotis() {
+    if (this.forma.value.slaptazodis != this.forma.value.slaptazodis2) {
+      alert("Slaptazodiai nesutampa !")
+    }
+    else {
+      console.log(this.forma.value)
+      this.apiService.registruotis(this.forma.value)
+      //localhost:44321/login
+    }
   }
 
 }
